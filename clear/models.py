90e0ecc8da96654f89c4ee568f76b900eb135272
Jimmy Oty <oduorjames@hotmail.com>
2020-10-21 02:20:38 +0300

    CLEARANCE APPS Create the Clearance apps: Students & Clear

---
+++ clear/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+class Department(models.Model):
+    department = models.CharField(max_length=100)
+    hod = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.department
+
+class Request(models.Model):
+    request = models.CharField(default="Clearance Request", max_length=100)
+    departments = models.ManyToManyField(Department)
+    date_posted = models.DateTimeField(default=timezone.now)
+    student = models.ForeignKey(User, on_delete=models.CASCADE) #change to OneToOneField to only have one request per user
+
+    def __str__(self):
+        #return f'{self.student.profile.Adm_no, self.student.first_name, self.request}'
+        return self.student.first_name +" "+ self.student.last_name+" "+ self.student.profile.Adm_no +" "+ self.request
+
+class Course(models.Model):
+    course = models.CharField(max_length=100)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.course 
+
+class Subject(models.Model):
+    subject = models.CharField(max_length=100)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.subject
--- clearance/settings.py
+++ clearance/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'clear',
+    'students',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
