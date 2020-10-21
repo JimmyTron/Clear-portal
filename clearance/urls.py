0ed147135ef2d7ef9eebba37d12c08a04f01c1a0
Jimmy Oty <oduorjames@hotmail.com>
2020-10-21 03:22:07 +0300

    CLEARANCE PORTAL Site Header and Site Title change from default

--- clearance/urls.py
+++ clearance/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from django.conf import settings
+#from django.conf.urls.static import static
 
+admin.site.site_header ="TKNP STUDENTS CLEARANCE PORTAL"
+admin.site.index_title ="Clearance Portal"
+admin.site.site_title ="Admin"
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+
 ]
